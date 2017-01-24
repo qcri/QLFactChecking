84264e4fed946fd695d90a12f4d661069dceb77c
Mitra Mohtarami <mitra@pooh.csail.mit.edu>
2017-01-24 11:36:10 -0500

    add linguistic features

--- code/classification/answers/RunMultiple.py
+++ code/classification/answers/RunMultiple.py
@@ -1,6 +1,20 @@
 import RunCV
 
 RUNS = [
+    ['LINGF_BIAS_LEX-incl', 'LINGF_BIAS_LEX-incl'], #linguistic Features
+    ['LINGF_HEDGES-incl', 'LINGF_HEDGES-incl'], #linguistic Features
+    ['LINGF_IMPLICATIVES-incl', 'LINGF_IMPLICATIVES-incl'], #linguistic Features
+    ['LINGF_ASSERTIVES-incl', 'LINGF_ASSERTIVES-incl'], #linguistic Features
+    ['LINGF_FACTIVES-incl', 'LINGF_FACTIVES-incl'], #linguistic Features
+    ['LINGF_REPORT_VERBS-incl', 'LINGF_REPORT_VERBS-incl'], #linguistic Features
+    ['LINGF_All_BIAS_LEX-incl', 'LINGF_All_BIAS_LEX-incl'], #linguistic Features
+    ['LINGF_STRONGSubj-incl', 'LINGF_STRONGSubj-incl'], #linguistic Features
+    ['LINGF_WEAKSubj-incl', 'LINGF_WEAKSubj-incl'], #linguistic Features
+    ['LINGF_NEGATIVES-incl', 'LINGF_NEGATIVES-incl'], #linguistic Features
+    ['LINGF_POSITIVES-incl', 'LINGF_POSITIVES-incl'], #linguistic Features
+    ['LINGF_MODALS-incl', 'LINGF_MODALS-incl'], #linguistic Features
+    ['LINGF_NEGATIONS-incl', 'LINGF_NEGATIONS-incl'], #linguistic Features
+    ['LINGF_All-incl', 'LINGF_All-incl'], #linguistic Features
     ['CATEGORIES-incl', 'CATEGORIES-incl'],
     ['QUALITY-incl', 'QUALITY-incl'],
     ['TROLLNESS-incl', 'TROLLNESS-incl'],
@@ -25,6 +39,8 @@ RUNS = [
     ['RANK_SAME_USER-excl', 'RANK_SAME_USER-excl'],
     ['VEC_COSINES_THREAD-excl', 'VEC_COSINES_THREAD-excl'],
     ['COSINES-excl', 'COSINES-excl'],
+    ['LINGF_All-excl', 'LINGF_All-excl'], #excluding all linguistic Features
+    ['LINGF_All_BIAS_ADDED_TO_OTHERS', 'LINGF_All_BIAS_ADDED_TO_OTHERS'], #including all linguistic bias Features
     ['all', 'all'],
 ]
 
