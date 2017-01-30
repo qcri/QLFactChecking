37f4b732837c24463bd3b4bd894a9fa349606fa8
gkaradjov <gogokaradjov@gmail.com>
2017-01-30 10:09:02 +0200

    Added runs

--- code/classification/answers/RunMultiple.py
+++ code/classification/answers/RunMultiple.py
@@ -1,38 +1,38 @@
 import RunCV
 
 RUNS = [
-    ['ARTICLES_SUPPORTS_BESTMATCH-incl', 'ARTICLES_SUPPORTS_BESTMATCH-incl'], #article supports
-    ['ARTICLES_SUPPORTS_ENTIREC-incl', 'ARTICLES_SUPPORTS_ENTIREC-incl'], #article supports
-    ['ARTICLES_SUPPORTS_All-incl', 'ARTICLES_SUPPORTS_All-incl'], #article supports
-    ['ARTICLES_SUPPORTS_BESTMATCH_LEVENSHTEIN-incl', 'ARTICLES_SUPPORTS_BESTMATCH_LEVENSHTEIN-incl'], #article supports
-    ['ARTICLES_SUPPORTS_ENTIREC_LEVENSHTEIN-incl', 'ARTICLES_SUPPORTS_ENTIREC_LEVENSHTEIN-incl'], #article supports
-    ['ARTICLES_SUPPORTS_All_LEVENSHTEIN-incl', 'ARTICLES_SUPPORTS_All_LEVENSHTEIN-incl'], #article supports
-    ['LINGF_BIAS_LEX-incl', 'LINGF_BIAS_LEX-incl'], #linguistic Features
-    ['LINGF_HEDGES-incl', 'LINGF_HEDGES-incl'], #linguistic Features
-    ['LINGF_IMPLICATIVES-incl', 'LINGF_IMPLICATIVES-incl'], #linguistic Features
-    ['LINGF_ASSERTIVES-incl', 'LINGF_ASSERTIVES-incl'], #linguistic Features
-    ['LINGF_FACTIVES-incl', 'LINGF_FACTIVES-incl'], #linguistic Features
-    ['LINGF_REPORT_VERBS-incl', 'LINGF_REPORT_VERBS-incl'], #linguistic Features
-    ['LINGF_All_BIAS_LEX-incl', 'LINGF_All_BIAS_LEX-incl'], #linguistic Features
-    ['LINGF_STRONGSubj-incl', 'LINGF_STRONGSubj-incl'], #linguistic Features
-    ['LINGF_WEAKSubj-incl', 'LINGF_WEAKSubj-incl'], #linguistic Features
-    ['LINGF_NEGATIVES-incl', 'LINGF_NEGATIVES-incl'], #linguistic Features
-    ['LINGF_POSITIVES-incl', 'LINGF_POSITIVES-incl'], #linguistic Features
-    ['LINGF_MODALS-incl', 'LINGF_MODALS-incl'], #linguistic Features
-    ['LINGF_NEGATIONS-incl', 'LINGF_NEGATIONS-incl'], #linguistic Features
-    ['LINGF_All-incl', 'LINGF_All-incl'], #linguistic Features
-    ['CATEGORIES-incl', 'CATEGORIES-incl'],
-    ['QUALITY-incl', 'QUALITY-incl'],
-    ['TROLLNESS-incl', 'TROLLNESS-incl'],
-    ['ACTIVITY-incl', 'ACTIVITY-incl'],
-    ['CREDIBILITY-incl', 'CREDIBILITY-incl'],
-    ['SENTIMENT-incl', 'SENTIMENT-incl'],
-    ['GOOGLE_VEC-incl', 'GOOGLE_VEC-incl'],
-    ['QL_VEC-incl', 'QL_VEC-incl'],
-    ['SYNTAX_VEC-incl', 'SYNTAX_VEC-incl'],
-    ['RANK_SAME_USER-incl', 'RANK_SAME_USER-incl'],
-    ['VEC_COSINES_THREAD-incl', 'VEC_COSINES_THREAD-incl'],
-    ['COSINES-incl', 'COSINES-incl'],
+    #['ARTICLES_SUPPORTS_BESTMATCH-incl', 'ARTICLES_SUPPORTS_BESTMATCH-incl'], #article supports
+    #['ARTICLES_SUPPORTS_ENTIREC-incl', 'ARTICLES_SUPPORTS_ENTIREC-incl'], #article supports
+    #['ARTICLES_SUPPORTS_All-incl', 'ARTICLES_SUPPORTS_All-incl'], #article supports
+    #['ARTICLES_SUPPORTS_BESTMATCH_LEVENSHTEIN-incl', 'ARTICLES_SUPPORTS_BESTMATCH_LEVENSHTEIN-incl'], #article supports
+    #['ARTICLES_SUPPORTS_ENTIREC_LEVENSHTEIN-incl', 'ARTICLES_SUPPORTS_ENTIREC_LEVENSHTEIN-incl'], #article supports
+    #['ARTICLES_SUPPORTS_All_LEVENSHTEIN-incl', 'ARTICLES_SUPPORTS_All_LEVENSHTEIN-incl'], #article supports
+    #['LINGF_BIAS_LEX-incl', 'LINGF_BIAS_LEX-incl'], #linguistic Features
+    #['LINGF_HEDGES-incl', 'LINGF_HEDGES-incl'], #linguistic Features
+    #['LINGF_IMPLICATIVES-incl', 'LINGF_IMPLICATIVES-incl'], #linguistic Features
+    #['LINGF_ASSERTIVES-incl', 'LINGF_ASSERTIVES-incl'], #linguistic Features
+    #['LINGF_FACTIVES-incl', 'LINGF_FACTIVES-incl'], #linguistic Features
+    #['LINGF_REPORT_VERBS-incl', 'LINGF_REPORT_VERBS-incl'], #linguistic Features
+    #['LINGF_All_BIAS_LEX-incl', 'LINGF_All_BIAS_LEX-incl'], #linguistic Features
+    #['LINGF_STRONGSubj-incl', 'LINGF_STRONGSubj-incl'], #linguistic Features
+    #['LINGF_WEAKSubj-incl', 'LINGF_WEAKSubj-incl'], #linguistic Features
+    #['LINGF_NEGATIVES-incl', 'LINGF_NEGATIVES-incl'], #linguistic Features
+    #['LINGF_POSITIVES-incl', 'LINGF_POSITIVES-incl'], #linguistic Features
+    #['LINGF_MODALS-incl', 'LINGF_MODALS-incl'], #linguistic Features
+    #['LINGF_NEGATIONS-incl', 'LINGF_NEGATIONS-incl'], #linguistic Features
+    #['LINGF_All-incl', 'LINGF_All-incl'], #linguistic Features
+    #['CATEGORIES-incl', 'CATEGORIES-incl'],
+    #['QUALITY-incl', 'QUALITY-incl'],
+    #['TROLLNESS-incl', 'TROLLNESS-incl'],
+    #['ACTIVITY-incl', 'ACTIVITY-incl'],
+    #['CREDIBILITY-incl', 'CREDIBILITY-incl'],
+    #['SENTIMENT-incl', 'SENTIMENT-incl'],
+    #['GOOGLE_VEC-incl', 'GOOGLE_VEC-incl'],
+    #['QL_VEC-incl', 'QL_VEC-incl'],
+    #['SYNTAX_VEC-incl', 'SYNTAX_VEC-incl'],
+    #['RANK_SAME_USER-incl', 'RANK_SAME_USER-incl'],
+    #['VEC_COSINES_THREAD-incl', 'VEC_COSINES_THREAD-incl'],
+    #['COSINES-incl', 'COSINES-incl'],
     # ['CATEGORIES-excl', 'CATEGORIES-excl'],
     # ['QUALITY-excl', 'QUALITY-excl'],
     # ['TROLLNESS-excl', 'TROLLNESS-excl'],
@@ -47,9 +47,12 @@ RUNS = [
     # ['COSINES-excl', 'COSINES-excl'],
     # ['LINGF_All-excl', 'LINGF_All-excl'], #excluding all linguistic Features
     # ['LINGF_All_BIAS_ADDED_TO_OTHERS', 'LINGF_All_BIAS_ADDED_TO_OTHERS'], #including all linguistic bias Features
-    ["IRF_Bing_snippets-incl", "IRF_Bing_snippets-incl"],
-    ["IRF_Google_snippets-incl", "IRF_Google_snippets-incl"],
-    ["IRF_ALL_snippets-incl", "IRF_ALL_snippets-incl"],
+    ["IRF_ALL_Bing_qataronly-incl", "IRF_ALL_Bing_qataronly-incl"],
+    ["IRF_ALL_webpages_qataronly_splitted-incl", "IRF_ALL_webpages_qataronly_splitted-incl"],
+    ["IRF_Bing_webpage_splitted-incl", "IRF_Bing_webpage_splitted-incl"],
+    ["IRF_ALL_Google_qataronly_splitted_others_bulk-incl", "IRF_ALL_Google_qataronly_splitted_others_bulk-incl"],
+    ["IRF_Google_snippets_qataronly_splitted_others_bulk-incl", "IRF_Google_snippets_qataronly_splitted_others_bulk-incl"],
+
     # ["IRFeatures-excl", "IRFeatures-excl"],
     # ['all', 'all'],
 ]
